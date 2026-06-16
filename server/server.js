e90ae6761744a8a138064900dc3d44f1010c5e6d
clintonkegakilwe-debug <clintonkegakilwe@gmail.com>
2026-06-16 21:26:18 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+const roomRoutes = require("./routes/rooms");
+const bookingRoutes = require("./routes/bookings");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/rooms", roomRoutes);
+app.use("/api/bookings", bookingRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Legacy Hotels API Running");
+    res.send("Legacy Hotels API Running");
 });
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
