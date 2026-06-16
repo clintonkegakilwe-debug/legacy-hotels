11883c7dda37424bcec1d0758d823885c197841d
clintonkegakilwe-debug <clintonkegakilwe@gmail.com>
2026-06-16 21:33:50 +0200

    Create pages/Rooms.jsx

---
+++ client/pages/Rooms.jsx
@@ -0,0 +1,18 @@
+function Rooms() {
+  return (
+    <div>
+      <h1>Our Rooms</h1>
+
+      <h3>Standard Room</h3>
+      <p>P800 per night</p>
+
+      <h3>Deluxe Room</h3>
+      <p>P1200 per night</p>
+
+      <h3>Executive Suite</h3>
+      <p>P2000 per night</p>
+    </div>
+  );
+}
+
+export default Rooms;
