8b55827a5ccb4c0081df3ca0dd282ab08f39ee21
clintonkegakilwe-debug <clintonkegakilwe@gmail.com>
2026-06-17 04:57:07 +0200

    Create components/Navbar.jsx

---
+++ client/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h1>Legacy Hotels</h1>
+
+      <ul>
+        <li>Home</li>
+        <li>Rooms</li>
+        <li>Book</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
